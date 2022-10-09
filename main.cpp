b7b0d374ab2ac6543486db656440e0ae4ede2c7c
dlwnsrb0829 <ijg0829@naver.com>
2022-10-09 16:32:55 +0900

    ADD : add the read file code and string tokenization code

--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
 #include<iostream>
+#include<fstream>
+#include<string.h>
 using namespace std;
 
 int main(){
-    cout << "Hello World" << endl;
+    ifstream readFile;
+    readFile.open("SAMPLE");
+    if(readFile.is_open()){
+        cout << "file is open!" << endl;
+        char arr[256];
+        while(!readFile.eof()){
+            readFile.getline(arr, 256);
+            cout << arr << endl;
+            char *token = strtok(arr, " ");
+            while(token != NULL){
+                cout << token << "" << endl;
+                token = strtok(NULL, " ");
+            }
+        }
+        
+    }
+    readFile.close();
+
     return 0;
 }
