244b9f82adb5ceaec6eb233aa6dd8aabd1c449d5
dlwnsrb0829 <ijg0829@naver.com>
2022-10-11 18:09:39 +0900

    UPDATE : change adj matrix type, int adj[][] -> int** adj

--- main.cpp
+++ main.cpp
@@ -1,14 +1,16 @@
 #include<iostream>
-#include<string>
+#include<cstring>
 #include<fstream>
 #include<sstream>
 #include<vector>
-#include <typeinfo>
+#include<typeinfo>
 #include"vertex.h"
 #include"edge.h"
 using namespace std;
 
 vector<string> split(string str, char delimiter);
+void DFS(int i, int visited[], int *matrix[]);
+void test(int **matrix, int size);
 
 int main(){
     vector<vertex> vertexs;
@@ -20,7 +22,7 @@ int main(){
         char arr[256];
         while(!readFile.eof()){
             readFile.getline(arr, 256);
-            cout << arr << endl;
+            // cout << arr << endl;
             vector<string> split_vector = split(arr, ' '); // split string
             if(split_vector[0] == "v"){
                 vertex v;
@@ -43,17 +45,45 @@ int main(){
     // for(int i = 0 ; i < edges.size() ; i++){
     //     cout << edges[i].getEdge()[0] << " " << edges[i].getEdge()[1] << endl;
     // }
-    int adjancency_matrix[vertexs.size()][vertexs.size()]; // initialization 2-d array to 0
-    memset(adjancency_matrix, 0, sizeof(adjancency_matrix));
-    for(int i = 0 ; i < edges.size() ; i++){
-        adjancency_matrix[edges[i].getEdge()[0]][edges[i].getEdge()[1]] = 1;
+
+
+    // int adjancency_matrix[vertexs.size()][vertexs.size()]; // initialization 2-d array to 0
+    // memset(adjancency_matrix, 0, sizeof(adjancency_matrix));
+    // for(int i = 0 ; i < edges.size() ; i++){
+    //     adjancency_matrix[edges[i].getEdge()[0]][edges[i].getEdge()[1]] = 1;
+    // }
+    // for(int i = 0 ; i < vertexs.size() ; i++){
+    //     for(int j = 0 ; j < vertexs.size() ; j++){
+    //         cout << adjancency_matrix[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // int visited[vertexs.size()];
+    // memset(visited, 0, sizeof(visited));
+
+    // cout << "test func" << endl;
+    // test(adjancency_matrix);
+
+    int **adj_matrix;
+    adj_matrix = new int *[vertexs.size()];
+    for(int i = 0 ; i < vertexs.size() ; i++){
+        adj_matrix[i] = new int[vertexs.size()];
     }
+
+    for(int i = 0 ; i < edges.size() ; i++){
+        adj_matrix[edges[i].getEdge()[0]][edges[i].getEdge()[1]] = 1;
+    }
+
     for(int i = 0 ; i < vertexs.size() ; i++){
         for(int j = 0 ; j < vertexs.size() ; j++){
-            cout << adjancency_matrix[i][j] << " ";
+            cout << adj_matrix[i][j] << " ";
         }
         cout << endl;
     }
+    
+    cout << "test" << endl;
+    test(adj_matrix, vertexs.size());
 
     return 0;
 }
@@ -66,4 +96,13 @@ vector<string> split(string input, char delimiter) {
         result.push_back(temp);
     }
     return result;
+}
+
+void test(int **matrix, int size){
+    for(int i = 0 ; i < size ; i++){
+        for(int j = 0 ; j < size ; j++){
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
 }
