924b516ca187ca365d9a64fb703561a18c49951f
dlwnsrb0829 <ijg0829@naver.com>
2022-10-12 19:12:00 +0900

    ADD : add DFS_use_stack method, add clock variable for realtime of recursion and stack

--- main.cpp
+++ main.cpp
@@ -3,13 +3,16 @@
 #include<fstream>
 #include<sstream>
 #include<vector>
+#include<stack>
 #include<typeinfo>
+#include<ctime>
 #include"vertex.h"
 #include"edge.h"
 using namespace std;
 
 vector<string> split(string str, char delimiter);
-void DFS(int i, int **matrix, bool visited[], int size);
+void DFS_use_recursion(int i, int **matrix, bool visited[], int size);
+void DFS_use_stack(int start, int** matrix, bool visited[], stack<int> stk, int size);
 
 int main(){
     vector<vertex> vertexs;
@@ -20,7 +23,6 @@ int main(){
         char arr[256];
         while(!readFile.eof()){
             readFile.getline(arr, 256);
-            // cout << arr << endl;
             vector<string> split_vector = split(arr, ' '); // split string
             if(split_vector[0] == "v"){
                 vertex v;
@@ -49,14 +51,29 @@ int main(){
         adj_matrix[edges[i].getEdge()[0]][edges[i].getEdge()[1]] = 1;
         adj_matrix[edges[i].getEdge()[1]][edges[i].getEdge()[0]] = 1;
     }
-    // for(int i = 0 ; i < vertexs.size() ; i++){
-    //     for(int j = 0 ; j < vertexs.size() ; j++){
-    //         cout << adj_matrix[i][j] << " ";
-    //     }
-    //     cout << endl;
-    // }
-    DFS(0, adj_matrix, visited, vertexs.size());
+    stack<int> stk;
+
+    for(int i = 0 ; i < vertexs.size() ; i++){
+        for(int j = 0 ; j < vertexs.size() ; j++){
+            cout << adj_matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    clock_t start1, start2, end1, end2;
+
+    cout << "use stack" << endl;
+    start1 = clock();
+    DFS_use_stack(0, adj_matrix, visited, stk, vertexs.size());
+    end1 = clock();
+    memset(visited, false, sizeof(bool) * vertexs.size());
+    cout << "\nuse recursion" << endl;
+    start2 = clock();
+    DFS_use_recursion(0, adj_matrix, visited, vertexs.size());
+    end2 = clock();
     cout << endl;
+    cout << "use stack time : " << (double)(end1 - start1) << "ms" << endl;
+    cout << "use recursion time : " << (double)(end2 - start2) << "ms" << endl;
     return 0;
 }
 
@@ -70,12 +87,33 @@ vector<string> split(string input, char delimiter) {
     return result;
 }
 
-void DFS(int u, int** matrix, bool visited[], int size){
+void DFS_use_recursion(int u, int** matrix, bool visited[], int size){
     cout << u << " ";
     visited[u] = true;
     for(int i = 0 ; i < size ; i++){
         if(matrix[u][i] == 1 && !visited[i]){
-            DFS(i, matrix, visited, size);
+            DFS_use_recursion(i, matrix, visited, size);
         }
     }
+}
+
+void DFS_use_stack(int start, int** matrix, bool visited[], stack<int> stk, int size) {
+	int current = start;
+	stk.push(current);
+	visited[current] = true;
+    cout << current<< " ";
+	while (stk.size()) {
+		current = stk.top();
+		for (int i = 0; i < size; i++) {
+			if (matrix[current][i] == 1 && !visited[i]) {
+				current = i;
+				stk.push(current);
+				visited[current] = true;
+                cout << current<< " ";
+                break;
+			}else if(i == size-1){
+                stk.pop();
+            }
+		}
+	}
 }
